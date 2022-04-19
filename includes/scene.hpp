9e3d8360a2907884b7f35c61a7fafa62b548e1eb
emacc <lalitoceja@gmail.com>
2022-04-19 12:39:49 -0500

    independant rotation for model and teapot

--- includes/scene.hpp
+++ includes/scene.hpp
@@ -1,5 +1,4 @@
 #pragma once
-// #define GL_GLEXT_PROTOTYPES 1
 
 #include <vector>
 #include <string>
@@ -11,21 +10,19 @@
     #include <OpenGL/glu.h>
     #include <GLUT/glut.h>
 #else
+
     #include <GL/gl.h>
     #include <GL/glu.h>
     #include <GL/glut.h>
 #endif
-// glewExperimental = GL_TRUE;
 #include <glm/glm.hpp>
 
+constexpr float rotating_factor = 0.5f;
 class Scene{
 
 public: 
     static void draw_roof(const GLuint *texture, const float &f);
     static void draw_floor(const GLuint *texture, const float &f);
-    static void draw_innerwall_1(const GLuint *texture);
-    static void draw_innerwall_2(const GLuint *texture);
-    static void draw_innerwall_3(const GLuint *texture);
     
     static void draw_teapot(const GLdouble size = 0.5);
 };
@@ -38,4 +35,5 @@ public:
 private:
     std::vector<GLfloat> vertexData;
     GLuint VAO, VBO, EBO; //vertex array obj, vertex buffer obj, 
+    float rotation = 0 ;
 };
--- src/scene.cpp
+++ src/scene.cpp
@@ -1,6 +1,7 @@
 #include "includes/scene.hpp"
 #include "includes/utils.hpp"
 #include "OBJ-Loader/Source/OBJ_Loader.h"
+float teapot_rotation = 0.f;
 
 void Scene::draw_floor(const GLuint *texture, const float &f){
     glEnable(GL_TEXTURE_2D);
@@ -27,11 +28,13 @@ void Scene::draw_floor(const GLuint *texture, const float &f){
 
 
 void Scene::draw_teapot(const GLdouble size){
-    glTranslatef(0.f, 0.f, 0.f);
-    glRotatef(.5f, 1, 1, 1);
-    glColor3f(1.f, .6f, 0.f);
-//    glScalef(1.f,1.f,1.f);
-    glutSolidTeapot(size);
+    teapot_rotation += rotating_factor;
+    glPushMatrix();
+        glTranslatef(0.7f, 0.f, 0.f);
+        glColor3f(1.f, .6f, 0.f);
+        glRotatef(teapot_rotation, 1, 1, 1);
+        glutSolidTeapot(size);
+    glPopMatrix();
 }
 
 
@@ -44,7 +47,7 @@ Model::Model(const std::string &filename){
     loader.LoadFile(path.string());
     objl::Mesh mesh = loader.LoadedMeshes.back();
 
-    for (int vertex = 0; vertex < mesh.Vertices.size(); ++vertex){
+    for (unsigned long vertex = 0; vertex < mesh.Vertices.size(); ++vertex){
         vertexData.push_back(mesh.Vertices[vertex].Position.X);
         vertexData.push_back(mesh.Vertices[vertex].Position.Y);
         vertexData.push_back(mesh.Vertices[vertex].Position.Z);
@@ -71,11 +74,13 @@ void Model::Draw(){
     glPushMatrix();
         glScalef(.5f, .5f, .5f);
         glColor3f(1,1,1);
+        glRotatef(rotation, 1,1,1);
         glDrawArrays(GL_TRIANGLES, 0, vertexData.size() / 3);
     glPopMatrix();
 
     glDisableClientState(GL_VERTEX_ARRAY);
     glDisableClientState(GL_NORMAL_ARRAY);
-    // glDrawElements(GL_TRIANGLES, 3, GL_UNSIGNED_SHORT, nullptr); //this causes a segfault, need to ask
+
+    rotation -= rotating_factor;
     
 }
